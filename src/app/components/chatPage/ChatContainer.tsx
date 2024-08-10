b450f667256c5551d57736059b58a202118a6a57
William Lowrimore <wlowrimore@gmail.com>
2024-08-09 19:07:51 -0500

    adds links to sidebar and updates the styles on borders...adjusts spacing y-axis sidebar

--- src/app/components/chatPage/ChatContainer.tsx
+++ src/app/components/chatPage/ChatContainer.tsx
@@ -5,14 +5,16 @@ import { ServiceDetailsProvider } from "@/providers/ServiceDetailsProvider";
 const ChatContainer = () => {
   return (
     <div className="w-full max-w-[80rem] min-h-screen bg-gray-800 flex flex-col justify-center px-12 text-white">
-      <div className="flex border-r w-full">
-        <aside className="max-w-[20rem] w-full flex flex-col px-6 border-r">
+      <div className="flex border-r border-gray-700 w-full">
+        <aside className="max-w-[20rem] w-full flex flex-col px-6 border-r border-gray-700">
           <ServiceDetailsProvider>
             <SideBar />
           </ServiceDetailsProvider>
         </aside>
-        <main className="flex-1 flex-col p-6">
-          <h1>Chat with KAL</h1>
+        <main className="bg-[url('/logos/site-logo-bg.webp')] bg-no-repeat bg-center flex-1 flex-col p-6">
+          <div className="w-full h-full bg-gray-800/95">
+            <h1>Chat with KAL</h1>
+          </div>
         </main>
       </div>
     </div>
--- src/app/components/chatPage/SideBar.tsx
+++ src/app/components/chatPage/SideBar.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-import Image from "next/image";
-import SiteLogo from "../../../../public/logos/site-logo.webp";
-import { MdOutlineLocationOn } from "react-icons/md";
-import { useServiceDetails } from "../../../providers/ServiceDetailsProvider";
-import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 import { useEffect } from "react";
+import { useSession, signOut } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import { useServiceDetails } from "../../../providers/ServiceDetailsProvider";
+import SiteLogo from "../../../../public/logos/site-logo.webp";
+
+import { RiHome2Line } from "react-icons/ri";
+import { MdOutlineLocationOn } from "react-icons/md";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { RiMailSendLine } from "react-icons/ri";
+import { RiArchiveStackLine } from "react-icons/ri";
+import { RiLogoutCircleLine } from "react-icons/ri";
 
 const SideBar = () => {
   const { data: session } = useSession();
@@ -23,7 +29,7 @@ const SideBar = () => {
 
   return (
     <div className="pt-6 flex flex-col min-h-screen">
-      <div className="flex items-center justify-start w-full gap-2 mx-auto">
+      <div className="flex items-center justify-start w-full gap-2 mx-auto my-8">
         <Image
           src={SiteLogo}
           alt="Site Logo"
@@ -32,41 +38,73 @@ const SideBar = () => {
           className="w-16 h-14 rounded-full"
         />
         <div className="flex flex-col space-y-[-0.4rem]">
-          <h1 className="text-3xl uppercase text-[#cba9f2]">kal</h1>
+          <h1 className="text-4xl uppercase text-[#cba9f2]">kal</h1>
           <p className="text-[0.6rem] uppercase text-neutral-300 tracking-widest">
             customer service expert
           </p>
         </div>
       </div>
 
-      <div className="w-full h-[0.025rem] bg-neutral-400 my-4"></div>
+      <div className="w-full h-[0.025rem] bg-gray-700 my-4"></div>
 
       {/* Sevice Details */}
       {serviceDetails && (
-        <div className="flex flex-col items-center justify-start w-full mx-auto mt-4 space-y-3">
-          <h3 className="text-xs text-neutral-300 uppercase font-semibold tracking-wider w-full flex justify-start mb-2">
+        <div className="flex flex-col items-center justify-start w-full mx-auto my-8 space-y-3">
+          <h3 className="text-sm text-neutral-300 uppercase font-semibold tracking-wider w-full flex justify-start mb-2">
             Service Details
           </h3>
-          <div className="flex flex-col w-full items-start">
-            <p className="flex items-center gap-1">
-              <IoMdCheckmarkCircleOutline />
+          <div className="flex flex-col w-full items-start pl-4">
+            <p className="flex items-center gap-1 mb-4">
+              <IoMdCheckmarkCircleOutline className="w-5 h-5 text-neutral-400" />
               <span className="text-sm text-neutral-400">
                 {storedServiceType}
               </span>
             </p>
           </div>
-          <div className="flex w-full items-center pb-4">
-            <p className="flex items-center gap-1">
-              <MdOutlineLocationOn />
+          <div className="flex w-full items-center pb-4 pl-4">
+            <p className="flex items-center gap-1 mb-4">
+              <MdOutlineLocationOn className="w-6 h-6 ml-[-0.15rem] text-neutral-400/75" />
               <span className="text-sm text-neutral-400">
                 {storedServiceLocation}
               </span>
             </p>
           </div>
-          <div className="w-full h-[0.025rem] bg-neutral-400 my-4"></div>
+          <div className="w-full h-[0.025rem] bg-gray-700 my-4"></div>
         </div>
       )}
 
+      {/* Links */}
+      <nav className="my-2 text-neutral-300">
+        <ul className="w-full flex flex-col gap-3">
+          <Link href="/">
+            <li className="flex items-center gap-2 text-neutral-400 hover:text-neutral-300 hover:bg-gray-700/30 rounded-full px-4 py-2 transition-all duration-200">
+              <RiHome2Line className="w-5 h-5" />
+              <span>Home</span>
+            </li>
+          </Link>
+          <Link href="#">
+            <li className="flex items-center gap-2 text-neutral-400 hover:text-neutral-300 hover:bg-gray-700/30 rounded-full px-4 py-2 transition-all duration-200">
+              <RiMailSendLine className="w-5 h-5" />
+              <span>Mailing List</span>
+            </li>
+          </Link>
+          <Link href="#">
+            <li className="flex items-center gap-2 text-neutral-400 hover:text-neutral-300 hover:bg-gray-700/30 rounded-full px-4 py-2 transition-all duration-200">
+              <RiArchiveStackLine className="w-5 h-5" />
+              <span>Archived Chats</span>
+            </li>
+          </Link>
+          <li
+            onClick={() => signOut({ callbackUrl: "/" })}
+            className="flex items-center gap-2 text-neutral-400 hover:text-neutral-300 hover:bg-gray-700/30 rounded-full px-4 py-2 transition-all duration-200 cursor-pointer"
+          >
+            <RiLogoutCircleLine className="w-5 h-5" />
+            <span>Sign Out</span>
+          </li>
+        </ul>
+      </nav>
+
+      {/* User Profile */}
       <div className="sticky bottom-10 mt-auto flex items-center justify-start w-full gap-2 mx-auto">
         <Image
           src={session?.user?.image!}
