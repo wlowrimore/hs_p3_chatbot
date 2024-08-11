29724466cae7742b03ca390feb6b98346521fc57
William Lowrimore <wlowrimore@gmail.com>
2024-08-11 18:30:41 -0500

    updates utils/lib file to not having useSession called, and moves logic to custom hook

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,6 @@
 
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { useSession } from "next-auth/react";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
